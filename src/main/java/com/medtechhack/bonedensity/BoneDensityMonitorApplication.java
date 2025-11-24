46b29adc6efbaacc9397e8f31261ad70f5ffe0aa
Grace Wang <wge1600@gmail.com>
2025-11-24 18:42:55 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/medtechhack/bonedensity/BoneDensityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.medtechhack.bonedensity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoneDensityMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoneDensityMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medtechhack/bonedensity/BoneDensityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medtechhack.bonedensity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoneDensityMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
