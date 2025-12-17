10e4f6a07264c25fedda888fef16520f9b193763
Grace Wang <wge1600@gmail.com>
2025-12-16 21:01:13 -0500

    Remove Spring Security login requirement for public access

--- src/main/java/com/medtechhack/bonedensity/config/CspFilter.java
+++ src/main/java/com/medtechhack/bonedensity/config/CspFilter.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-public class CspFilter implements Filter{
+public class CspFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -15,9 +15,12 @@ public class CspFilter implements Filter{
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         httpResponse.setHeader("Content-Security-Policy",
                 "default-src 'self'; " +
-                "script-src 'se;f' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; " +
-                "style-src 'self' 'unsafe-inline'; " +
-                "img-src 'self' data: https::");
+                        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; " +
+                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+                        "font-src 'self' https://fonts.gstatic.com; " +
+                        "img-src 'self' data: https:; " +
+                        "connect-src 'self';");
+
         chain.doFilter(request, response);
     }
 }
